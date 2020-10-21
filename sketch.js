01682b8f5ce55268a07bb52a411a2598fd83d6d2
sudhakodali06 <72756905+sudhakodali06@users.noreply.github.com>
2020-10-21 22:23:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var speed, weight;
 function setup() {
 createCanvas(1600,400);
 
-speed = 50;
+speed = random(50,95);
 weight = random(400,1500);
 
 car = createSprite(80, 200, 50, 50);
@@ -26,14 +26,17 @@ function draw() {
       var deformation = (0.5*weight*speed*speed)/22500;
       
       if(deformation < 100){
+       car.x=1310;
         car.shapeColor="green";
       }
 
       if(deformation >=100 && deformation<=180){
+       car.x=1310;
         car.shapeColor = "yellow";
       }
 
       if(deformation > 180){
+       car.x=1310;
          car.shapeColor = "red";
       }
     }
